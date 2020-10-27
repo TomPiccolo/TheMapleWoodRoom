dffaccabe0659babab6de7dd32f3b2a68e4b086a
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2020-10-27 14:53:15 -0400

    Skybox & Frozen Pond

---
+++ TheMapleWoodRoom/Assets/Plugins/signin.bat
@@ -0,0 +1,2 @@
+title Sign In to AltspaceVR
+curl -v -d "user[email]=tomtompik@gmail.com&user[password]=0454VentureOut" https://account.altvr.com/users/sign_in.json -c cookie
---
+++ TheMapleWoodRoom/Assets/Plugins/upload.bat
@@ -0,0 +1,1 @@
+curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/thomaspiccolo/Documents/GitHub/TheMapleWoodRoom/TheMapleWoodRoom/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1592943491388801291.json
